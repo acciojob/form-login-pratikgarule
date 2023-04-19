8fcb6089a57f46408e5babf0c3086ab3fae7c786
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-19 10:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getFormvalue() {
     //Write your code here
- var first, second;
-    first = document.getElementById("fname").value;
-    second = document.getElementById("lname").value;
-
-    document.GetElementById("here").InnerHTML = first;
-    document.GetElementById("here").InnerHTML = second;
+var firstName = "John";
+var lastName = "Mark Doe";
+var firstName, lastName, fullName;
+firstName = 'John';
+lastName = 'Mark Doe';
+fullName = 'firstName' + 'lastName';
 }
